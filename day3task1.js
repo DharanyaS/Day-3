6c596cfbeb9ccae48163be53a6bbcd98c26609ff
DharanyaS <109056167+DharanyaS@users.noreply.github.com>
2022-07-22 05:29:15 +0530

    Add files via upload

---
+++ day3task1.js
@@ -0,0 +1,17 @@
+// for loop
+for(let i =0; i < 50; i++){
+    console.log("Number #", i);
+}
+
+// for in loop
+const person = {
+    name: 'Dharanya',
+    age: 24
+
+};
+
+for(let key in person){
+    //code
+    console.log("key: ", key);
+}
+
---
+++ day3task2.js
@@ -0,0 +1,58 @@
+{
+    "basics": {
+      "name": "Dharanya S",
+      "label": "Programmer",
+      "email": "dharanya2802@gmail.com",
+      "phone": "9986182673",
+      "degree": "MBA",
+      }
+      
+
+
+    "work": [
+      {
+        "company": "Huron consulting group",
+        "position": "Sinior process analyst",
+        "startDate": "3/3/2022"
+      }
+    ]
+    
+    "education": [
+      {
+        "institution": "Shushruti Institute of bussiness management",
+        "area": "Finance and HR",
+        "startDate": "2019",
+        "endDate": "2021",
+      }
+    ]
+
+    "awards": [
+      {
+        "title": "first place in food compitation",
+        "date": "2019",
+      }
+    ]
+
+    "skills": [
+      {
+        "name": "Full stack Developer",
+        "level": "beginner",
+        "keywords": [
+          "Keywords under this category (e.g. 'Java', 'C++', etc)"
+        ]
+      }
+    ]
+
+    "languages": [
+      {
+        "language": "Tamil",
+        "fluency": "Tamil, kannada, telugu, malayalam, hindi "
+      }
+    ]
+
+    "interests": [
+      {
+        "name": "desining works and cooking",
+      }
+    ]
+}
